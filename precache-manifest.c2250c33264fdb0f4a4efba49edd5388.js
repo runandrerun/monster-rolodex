274b68bc1d5fd29a5635806855460377883fbdac
runandrerun <the.asantiagojr@gmail.com>
2020-01-11 20:29:56 -0500

    Updates

---
+++ precache-manifest.c2250c33264fdb0f4a4efba49edd5388.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8dd970ed52b46e454cd8dc291d32d29",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "231914a389308348c650",
+    "url": "/monster-rolodex/static/css/main.94c0cb28.chunk.css"
+  },
+  {
+    "revision": "ea780a86624c6f10e78a",
+    "url": "/monster-rolodex/static/js/2.edc771fc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.edc771fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "231914a389308348c650",
+    "url": "/monster-rolodex/static/js/main.879e25c0.chunk.js"
+  },
+  {
+    "revision": "7eb3c49c2062c036820e",
+    "url": "/monster-rolodex/static/js/runtime-main.f242a4ea.js"
+  }
+]);
